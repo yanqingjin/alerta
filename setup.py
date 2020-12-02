930f60e0621f5ad51cea18d58b93325e8bb3b1bd
Edward Guan <285006386@qq.com>
2020-12-02 17:29:43 +0800

    update webhooks

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setuptools.setup(
             'grafana = alerta.webhooks.grafana:GrafanaWebhook',
             'graylog = alerta.webhooks.graylog:GraylogWebhook',
             'hsdp = alerta.webhooks.hsdp:HsdpWebhook',
+            'cloudmonitor = alerta.webhooks.cloudmonitor:CloudMonitorWebhook',
             'newrelic = alerta.webhooks.newrelic:NewRelicWebhook',
             'pagerduty = alerta.webhooks.pagerduty:PagerDutyWebhook',
             'pingdom = alerta.webhooks.pingdom:PingdomWebhook',
